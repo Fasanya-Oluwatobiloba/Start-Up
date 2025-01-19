535241f191490ebb58693b178ea8d1cb61a17116
Your Name <you@example.com>
2025-01-19 03:55:14 +0100

    edited the auth pages

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -61,7 +61,7 @@ function Login() {
           <div>
             <button
               type="submit"
-              className="w-full px-4 py-2 text-lg font-normal text-white bg-blue-600 border border-transparent"
+              className="w-full px-4 py-2 text-lg font-normal text-white bg-black border border-transparent"
             >
               Sign in
             </button>
--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -61,7 +61,7 @@ function Signup() {
 			<div>
 			  <button
 				type="submit"
-				className="w-full px-4 py-2 text-lg font-normal text-white bg-blue-600 border border-transparent"
+				className="w-full px-4 py-2 text-lg font-normal text-white bg-black border border-transparent"
 			  >
 				Sign up
 			  </button>
